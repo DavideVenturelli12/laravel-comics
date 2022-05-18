be471d1539c67f58819713ba82a8c5d82a54e5e6
Davide Venturelli <dventurelli90@gmail.com>
2022-05-18 17:22:29 +0200

    header finito

---
+++ laravel-comics/resources/views/home.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Laravel-comics</title>
+</head>
+<style>
+    * {
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+        font-family: sans-serif;
+    }
+
+</style>
+
+<body>
+    @include('partials/header')
+    @include('partials/main')
+    @include('partials/footer')
+</body>
+
+</html>
---
+++ laravel-comics/resources/views/partials/header.blade.php
@@ -0,0 +1,62 @@
+<header>
+    <div>
+        <img src="/img/dc-logo.png" alt="DC-Logo" />
+    </div>
+    <nav>
+        <ul>
+            <li><a href="/">CHARACTERS</a></li>
+            <li><a href="/">COMICS</a></li>
+            <li><a href="/">MOVIES</a></li>
+            <li><a href="/">TV</a></li>
+            <li><a href="/">GAMES</a></li>
+            <li><a href="/">COLLECTIBLES</a></li>
+            <li><a href="/">VIDEOS</a></li>
+            <li><a href="/">FANS</a></li>
+            <li><a href="/">NEWS</a></li>
+            <li><a href="/">SHOP</a></li>
+        </ul>
+    </nav>
+    <div>
+        <input type="text" placeholder="Search">
+        <i class="fa-solid fa-magnifying-glass"></i>
+    </div>
+</header>
+
+<!--tag style temporaneo-->
+<style>
+    header {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        padding: 10px 15%;
+    }
+
+    header img {
+        height: 70px;
+    }
+
+    li {
+        list-style: none;
+        display: inline-block;
+        margin: 0 10px
+    }
+
+    li a {
+        text-decoration: none;
+        color: black;
+        font-weight: 600;
+        padding-bottom: 35px;
+    }
+
+    a:hover {
+        border-bottom: 4px solid #0282f9;
+    }
+
+    input {
+        width: 150px;
+        border: 0;
+        border-bottom: 3px solid #0282f9;
+        text-align: right;
+    }
+
+</style>
